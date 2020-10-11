1be945d6e64dbbe64d30dc5588d360c9a56e6708
lealclarissa <lealclarissa2@gmail.com>
2020-10-11 18:36:25 -0300

    Added project structure

---
+++ src/routes/livrosRoute.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/livrosController');
+
+router.get('/', controller.getAllBooks);
+router.get('/:id', controller.getById);
+router.post('/', controller.postBooks);
+router.delete('/:id', controller.deleteBooks)
+
+module.exports = router;
