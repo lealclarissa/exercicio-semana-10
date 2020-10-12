477acd57f460a6f21602845d3b64749d2b982378
lealclarissa <lealclarissa2@gmail.com>
2020-10-12 19:11:50 -0300

    server.js adjustment

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./src/app');
 const port = 8083;
 
 app.listen(port, () => {
-    console.log(`App está rodando na porta ${port}`)
+    console.log(`App está rodando na porta: ${port}`)
 });
